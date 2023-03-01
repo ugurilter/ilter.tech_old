f8852b273578341171d102b4bedfe6fc8d4fe354
Ugur ILTER <ugur@ilter.tech>
2023-03-01 16:30:35 +0300

    clean up :wastebasket: and add contact page :memo:

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,23 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+/**
+ * Global constants
+ */
+export const SITE_TITLE       = "Ugur Ilter - Personal Website";
+export const SITE_DESCRIPTION = "Welcome to my personal website !";
+export const MY_NAME          = "Ugur Ilter";
+export const MY_TITLE         = "Sr. Embedded Software Engineer";
 
-export const SITE_TITLE = "Ugur Ilter - Personal Website";
-export const SITE_DESCRIPTION =
-  "Welcome to my personal website !";
-export const TWITTER_HANDLE = "@";
-export const MY_NAME = "Ugur Ilter";
+/**
+ * Social handles & urls
+ */
+export const SOC_LINKEDIN_HDL = "ugurilter"
+export const SOC_LINKEDIN_URL = "https://www.linkedin.com/in/ugurilter/"
+export const SOC_STEAM_HDL    = ""
+export const SOC_STEAM_URL    = ""
+export const SOC_GITHUB_HDL   = "ugurilter"
+export const SOC_GITHUB_URL   = "https://www.github.com/ugurilter"
+export const SOC_SO_HDL       = "ugurilter"
+export const SOC_SO_URL       = "https://stackoverflow.com/users/8676955/ugur-ilter"
+
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
